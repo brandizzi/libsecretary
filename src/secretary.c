95cf0a2487441a5365b0f8948cbe46d187e852ac
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-07-05 00:48:33 -0300

    now showing licences

--- src/secretary.c
+++ src/secretary.c
@@ -1,3 +1,24 @@
+
+/**
+ * libsecretary: a C library for managing to-do lists
+ * Copyright (C) 2011  Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can get the latest version of this file at 
+ * http://bitbucket.org/brandizzi/libsecretary/
+ */
 #include <secretary/secretary.h>
 #include <secretary/_internal/secretary.h>
 #include <secretary/_internal/task.h>
