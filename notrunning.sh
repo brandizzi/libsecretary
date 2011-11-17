6742c28dd43567167e1a7506ab3fe7536a3cd585
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-11-17 16:02:55 -0200

    Now using autotools - next step: libtools. YAY

--- notrunning.sh
+++ notrunning.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-find  src/test/ | xargs grep 'CuTest \*' | wc -l
-find  src/test/ | xargs grep 'SUITE_ADD_TEST' | wc -l
-find  src/test/ | xargs sed -n 's/.*void *\(test_.*\)(.*/\1/p' > exists
-find  src/test/ | xargs sed -n '/SUITE_ADD_TEST(suite, *$/{n;s/\n */ /;};s/.*SUITE_ADD_TEST(suite, \(.*\)).*/\1/p'  > runs
+find  test/ | xargs grep 'CuTest \*' | wc -l
+find  test/ | xargs grep 'SUITE_ADD_TEST' | wc -l
+find  test/ | xargs sed -n 's/.*void *\(test_.*\)(.*/\1/p' > exists
+find  test/ | xargs sed -n '/SUITE_ADD_TEST(suite, *$/{n;s/suite, *test/suite, test/;};s/.*SUITE_ADD_TEST(suite, \(.*\)).*/\1/p'  > runs
 sort exists > sorted_exists
 sort runs > sorted_runs
 diff sorted_exists sorted_runs
